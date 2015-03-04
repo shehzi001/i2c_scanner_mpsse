386bced1fbd5e96fa4e5be12a2da2b319cd418d9
Zeno <matthias.fueller.brsu+zeno@gmail.com>
2015-03-04 07:52:24 -0600

    fixed undefined ACK variable error

--- main.c
+++ main.c
@@ -11,13 +11,14 @@
 
 #define WCMD	"\xA0\x00\x00"	// Write start address command
 #define RCMD	"\xA1"		// Read command
+#define ACK 0
 
 int main(int argc, char **argv) {
 
 	struct mpsse_context *scan = NULL;
 	//scan = MPSSE(I2C, ONE_HUNDRED_KHZ, MSB);
 
-	if((scan = MPSSE(I2C, FOUR_HUNDRED_KHZ, MSB)) != NULL && scan->open)
+	if((scan = MPSSE(I2C, FOUR_HUNDRED_KHZ, LSB)) != NULL && scan->open)
 	{
 		int addr;
 		for (addr = 0; addr < 0x7F; ++addr)
