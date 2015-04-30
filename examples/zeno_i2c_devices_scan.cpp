f58ca9d0fc7858b5f53c4eee52442305da1a7307
Zeno <matthias.fueller.brsu+zeno@gmail.com>
2015-04-30 10:33:33 -0500

    added code to read ID's of IMU components

--- examples/zeno_i2c_devices_scan.cpp
+++ examples/zeno_i2c_devices_scan.cpp
@@ -28,24 +28,46 @@ int main(int argc, char **argv) {
 
     std::cout << "Zeno I2C interface started" << std::endl;
 
+    signed int data;
+    bool success = false;
     if(zeno_i2c_interface.initilizeI2CInterface()) {
         std::cout << "(null) initialized at 400000Hz (I2C)" << std::endl;
-           unsigned int data;
-           std::cout << "===================Reading Accelerometer=======================" << std::endl;
-           bool success = false; 
-           success = zeno_i2c_interface.readDevice(0x53, 0x31, &data);
-           if(success)
-             std::cout << "i2c read data: "  << std::hex << data << std::endl; 
-           std::cout << "===================Reading Gyroscope=======================" << std::endl;
+        sleep(1.0);
+        // Reading a dummy byte.
+        success = zeno_i2c_interface.readDevice(0x1E, 0x02, &data);
+        success = zeno_i2c_interface.readDevice(0x69, 0x20, &data);
+        success = zeno_i2c_interface.readDevice(0x53, 0x00, &data);
+        usleep(10.0);
+        for(int i=0; i<10;i++) {
+           data = 5;
            success = false;
-             success = zeno_i2c_interface.readDevice(0x69, 0x20, &data);
-           if(success)
-             std::cout << "i2c read data: "  << std::hex << data << std::endl;
 
-           std::cout << "===================Reading Compass=======================" << std::endl;
-           zeno_i2c_interface.readDevice(0x1E, 0x02, &data);
-           if (success)
-              std::cout << "i2c read data: "  << std::hex << data << std::endl;
+           std::cout << "===================Reading IMU=======================" << std::endl;
+           success = zeno_i2c_interface.readDevice(0x53, 0x00, &data);
+           if(success) {
+             std::cout << "Acceleromerter ID:"  << std::hex << data << std::endl;
+           } else {
+             std::cout << "Accelerometer read failed."  << std::endl;
+           }
+           usleep(10.0); 
+           success = false;
+           success = zeno_i2c_interface.readDevice(0x69, 0x20, &data);
+         
+           if(success) {
+             std::cout << "Gyroscope ID:"  << std::hex << data << std::endl;
+           } else {
+             std::cout << "Gyroscope read failed."  << std::endl;
+           }
+           usleep(10.0);
+           success = zeno_i2c_interface.readDevice(0x1E, 0x02, &data);
+         
+           if(success) {
+             std::cout << "Compass ID:"  << std::hex << data << std::endl;
+           } else {
+             std::cout << "Compass read failed."  << std::endl;
+           }
+           usleep(10.0);
+      }
        zeno_i2c_interface.closeI2CInterface();
     }
 
