b2b010faeed9cb69b7f890e7da1258f820e5c481
Zeno <matthias.fueller.brsu+zeno@gmail.com>
2015-05-08 08:15:39 -0500

    committing working version of data fusion from accel. and gyro

--- examples/zeno_imu_sensor_fusion.cpp
+++ examples/zeno_imu_sensor_fusion.cpp
@@ -14,8 +14,8 @@
 class IMUIntegration
 {
   public:
-      IMUIntegration(const ZenoI2CInterface &zeno_i2c_interface):
-        accel_(zeno_i2c_interface), gyro_(zeno_i2c_interface)
+      IMUIntegration(ZenoI2CInterface &zeno_i2c_interface):
+        gyro_(zeno_i2c_interface),  accel_(zeno_i2c_interface)
       {
       }
 
@@ -29,7 +29,7 @@ class IMUIntegration
         roll.resize(3);
         pitch.resize(3);
         yaw.resize(3);
-        gyro_.init(-1.3, 0.40, 0.669669);//-1.22858, -0.383618, -0.575575);//(-0.18891-0.04382, -0.09454-0.05530, -0.31720-0.04951); //Don’t  use my ca
+        gyro_.init(-1.3, 0.40, 0.669669);
         //gyro.printCalibrationValues(1000);
 
         angleF_roll = 0.0;
@@ -42,10 +42,10 @@ class IMUIntegration
         init();
         int i=0;
         pT = clock();
-        while(i<1000) {
+        while(i<200) {
+              i++;     
+              sleepcp(100);
               loop();
-              i++;
-              sleepcp(10);
         }
          //std::cout << "IMU accelerometer interface exiting" << std::endl;
       }
@@ -56,15 +56,11 @@ class IMUIntegration
         AccelRotation accelRot;
    
         accelRot = accel_.readPitchRoll();
-        /*
-        AccelG accelG;
-        accelG = accel_.readAccelG();
-        std::cout << "[" << accelG.x << "," << accelG.y << ","<< accelG.z << "," 
-                    << accelRot.roll << "," << accelRot.pitch << "]" << std::endl;
-        */
+        // std::cout << "[" << accelRot.roll << "," << accelRot.pitch << "]" << std::endl; 
         //END ACCEL
 
         //GYRO
+        
         clock_t cT = clock();
 
         GyroDPS gDPS;
@@ -72,23 +68,19 @@ class IMUIntegration
          
         unsigned long dT = cT - pT;
         pT = cT;
-
-        angleF_roll = 0.95*(angleF_roll + gDPS.x*0.03) +0.05*accelRot.roll;
-        angleF_pitch = 0.95*(angleF_pitch - gDPS.y*0.03) +0.05*accelRot.pitch;
-        
-        /*
+         
         double roll_c = gDPS.x*((double)dT/CLOCKS_PER_SEC);
         double pitch_c =  gDPS.y*((double)dT/CLOCKS_PER_SEC);
         double yaw_c = gDPS.z*((double)dT/CLOCKS_PER_SEC);
 
 
-        
+         
         // simple integrator
-        xi = xi + gDPS.x*0.03;//((double)dT/CLOCKS_PER_SEC);
-        yi = yi + gDPS.y*0.03;//((double)dT/CLOCKS_PER_SEC); 
-        zi = zi + gDPS.z*0.03;//((double)dT/CLOCKS_PER_SEC);
+        xi = xi + roll_c;//gDPS.x*0.03;//((double)dT/CLOCKS_PER_SEC);
+        yi = yi + pitch_c;//gDPS.y*0.03;//((double)dT/CLOCKS_PER_SEC); 
+        zi = zi + yaw_c;//gDPS.z*0.03;//((double)dT/CLOCKS_PER_SEC);
         
-        
+        /*
         //runge-kutta integration 
         xi = xi + (1/6)*(roll[2]+(2*roll[1])+(2*roll[1])+roll_c);
         yi = yi + (1/6)*(pitch[2]+(2*pitch[1])+(2*pitch[1])+pitch_c); 
@@ -104,10 +96,14 @@ class IMUIntegration
         yaw[2] = yaw[1];
         yaw[1] = yaw[0];
         yaw[0] = yaw_c;
-        
-        std::cout << "[" << gDPS.x << "," << gDPS.y << ","<< gDPS.z << "," 
-                      << xi << "," << yi << "," << zi << "]" << std::endl; 
         */
+        //std::cout << "[" << gDPS.x << "," << gDPS.y << ","<< gDPS.z << "," 
+
+      
+        angleF_roll = 0.95*(angleF_roll + roll_c) +0.05*accelRot.roll;
+        angleF_pitch = 0.95*(angleF_pitch - pitch_c) +0.05*accelRot.pitch;
+        
+        std::cout << "[" << xi << "," << yi << "," << accelRot.roll << "," << accelRot.pitch <<  "," << angleF_roll << "," << angleF_pitch << "]" << std::endl; 
         //END GYRO
       }
 
@@ -131,8 +127,8 @@ class IMUIntegration
          */
         IMUIntegration &operator=(const IMUIntegration &other);
 
-        ADXL345 accel_;
         L3G4200D gyro_;
+        ADXL345 accel_;
         double xi,yi,zi;
         clock_t pT;
         std::vector<double> roll;
@@ -141,6 +137,7 @@ class IMUIntegration
 
         double angleF_roll;
         double angleF_pitch;
+        ZenoI2CInterface zeno_i2c_interface_;
 };
 
 int main(int argc, char **argv) 
